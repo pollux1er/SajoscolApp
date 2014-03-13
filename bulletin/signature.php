395c30667ab59c6b24822d6558d35e48502d0c5e
Patient Assontia <assontia@gmail.com>
2014-03-13 13:25:11 +0100

    Initialisation 1

---
+++ bulletin/signature.php
@@ -0,0 +1,3 @@
+<div  id="signature" style="left: 450px;position: absolute;    top: 740px;    z-index: -99;">
+	<img src="../images/signature.png" height="160px" />
+</div>
---
+++ bulletin/signature2.php
@@ -0,0 +1,3 @@
+<div  id="signature" style="left: 450px;position: absolute;    top: 480px;    z-index: -999;">
+	<img src="../images/signature.png" height="160px" />
+</div>
